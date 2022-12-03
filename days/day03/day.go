67be82be74a054f9ce6abe2ab627d13b2468e786
Oleg Neizhkasha <oleg.neizhkasha@takeoff.com>
2022-12-03 11:50:54 +0200

    Added day03

---
+++ days/day03/day.go
@@ -0,0 +1,69 @@
+package day03
+
+import (
+	"fmt"
+	"strings"
+
+	. "github.com/panshdw/aoc2022/utils"
+)
+
+func init() {
+	RegisterTask("03/01", Task01, "03/01.twitter")
+	RegisterTask("03/02", Task02, "03/01.twitter")
+}
+
+func priority(item rune) int {
+	if item >= 'A' && item <= 'Z' {
+		return int(27+item - 'A' )
+	} else {
+		return int(1+item-'a')
+	}
+}
+
+func getCommonItems(c1, c2 string, firstOnly bool) []rune {
+	out := []rune{}
+	for _, r := range []rune(c2) {
+		if strings.IndexRune(c1, r) > -1 {
+			if firstOnly {
+				return []rune{r}
+			} else if strings.IndexRune(string(out), r) == -1 {
+				out = append(out, r)
+			}
+		}
+	}
+	return out
+}
+
+func Task01(input []string) string {
+	var sum int
+	for i, rucksack := range input {
+		size := len(rucksack)
+		half := size >> 1
+		fmt.Printf("%d rucksacks contains %d (%s %d + %s %d) items\n", i, size, rucksack[:half], half, rucksack[half:], half)
+		items := getCommonItems(rucksack[:half], rucksack[half:], true)
+		if items != nil {
+			itemp := priority(items[0])
+			sum += itemp
+			fmt.Printf("Item %c appears in both comparments: +%d = %d\n", items[0], itemp, sum)
+		}
+	}
+	return fmt.Sprintf("%d", sum)
+}
+
+func getBadgeOfGroup(rucksacks []string) rune {
+	common := getCommonItems(rucksacks[0], rucksacks[1], false)
+	items := getCommonItems(rucksacks[2], string(common), true)
+	return items[0]
+}
+
+func Task02(input []string) string {
+	var sum int
+	for i:=0; i<len(input); i+=3 {
+		fmt.Printf("Group:\n %s\n",strings.Join(input[i:i+3],"\n"))
+		item := getBadgeOfGroup(input[i:i+3])
+		itemp := priority(item)
+		sum += itemp
+		fmt.Printf("Common item in group: %c +%d = %d\n", item, itemp, sum )
+	}
+	return fmt.Sprintf("%d", sum)
+}
---
+++ days/day03/day_test.go
@@ -0,0 +1,33 @@
+package day03
+
+import (
+	"testing"
+
+	. "github.com/panshdw/aoc2022/utils"
+)
+
+var (
+	input = SplitText(`#
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+`)
+)
+
+func TestPriority(t *testing.T) {
+	Is(t, priority('a'), 1)
+	Is(t, priority('A'), 27)
+	Is(t, priority('z'), 26)
+	Is(t, priority('Z'), 52)
+}
+
+func TestTask01(t *testing.T) {
+	Is(t, Task01(input), "157")
+}
+
+func TestTask02(t *testing.T) {
+	Is(t, Task02(input), "70")
+}
