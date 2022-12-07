82911f7f7d86814b4e2cb120e4d1eeef0b82b400
Oleg Neizhkasha <panshdw@gmail.com>
2022-12-07 08:24:02 +0200

    Init day07

---
+++ days/day07/day07.go
@@ -0,0 +1,19 @@
+package day06
+
+import (
+	. "github.com/panshadow/aoc2022/utils"
+)
+
+func init() {
+	RegisterTask("06/01", Task01, "06/01.twitter")
+	RegisterTask("06/02", Task02, "06/01.twitter")
+}
+
+
+func Task01(input []string) string {
+	return ""
+}
+
+func Task02(input []string) string {
+	return ""
+}
