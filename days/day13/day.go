c4cb5bee4589510fa7489df5949a5ed18ee5dc98
Oleg Neizhkasha <oleg.neizhkasha@takeoff.com>
2022-12-13 09:34:51 +0200

    Initial day13

---
+++ days/day13/day.go
@@ -0,0 +1,19 @@
+package day13
+
+import (
+	. "github.com/panshadow/aoc2022/utils"
+)
+
+func init() {
+	RegisterTask("13/01", Task01, "13/01.twitter")
+	RegisterTask("13/02", Task02, "13/01.twitter")
+}
+
+
+func Task01(input []string) string {
+	return ""
+}
+
+func Task02(input []string) string {
+	return ""
+}
